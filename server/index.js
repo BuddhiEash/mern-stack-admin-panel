621e09163d8f29faa037fe4e43b973d756ab0da1
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-09-08 23:09:08 +0530

    A small modification added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ dotenv_1.default.config();
 var app = (0, express_1.default)();
 var port = process.env.PORT;
 app.get('/', function (req, res) {
-    res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server!!');
 });
 app.listen(port, function () {
     console.log("\u26A1\uFE0F[server]: Server is running at http://localhost:".concat(port));
