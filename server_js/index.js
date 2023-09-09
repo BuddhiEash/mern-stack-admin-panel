e16037bed86380ddcda0d8d895e8aeaf1dca4bc1
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-09-09 18:43:33 +0530

    Mysql connection created

--- server_js/index.js
+++ server_js/index.js
@@ -2,13 +2,30 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.PORT;
+const mysql = require('mysql');
+const app_port = process.env.APP_PORT;
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
+connection.connect();
+
+connection.query('SHOW DATABASES', (err, rows, fields) => {
+    if (err) throw err
+  
+    console.log('The solution is: ', rows)
+  })
+  
+connection.end()
 
 app.get('/', (req, res) => {
-  console.log('Request is :',req);
   res.send('ExpressJS Server!!')
 })
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+app.listen(app_port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${app_port}`)
 })
