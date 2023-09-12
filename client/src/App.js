024a4021c0c58df14ec75488adf75d9f544d409d
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-09-12 15:17:23 +0530

    login page created with custom css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import '@picocss/pico'
+import './App.css';
 
 function App() {
   return (
-      <main class="container">
-        <h1>Hello, world with Pico CSS!</h1>
+      <main class="container-fluid login-page">
+        <h1 align="center">Login</h1>
+        <form>
+        <label for="email">Username</label>
+        <input type="text" id="username" name="username" placeholder="JohnDoe_404" required></input>
+        <label for="email">Password</label>
+        <input type="password" id="password" name="password" placeholder="Password" required></input>
+        <small>Don't have an account? <a href='#'>Register</a></small>
+        <button type="submit">Login</button>
+        </form>
       </main>
   );
 }
