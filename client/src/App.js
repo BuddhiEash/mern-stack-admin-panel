00373f0f71bf03c624a0db68e3b4c01476b2c6f4
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-09-12 15:20:44 +0530

    title updated

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 function App() {
   return (
       <main class="container-fluid login-page">
-        <h1 align="center">Login</h1>
+        <h1 align="center">Admin Panel</h1>
         <form>
         <label for="email">Username</label>
-        <input type="text" id="username" name="username" placeholder="JohnDoe_404" required></input>
+        <input type="text" id="username" name="username" placeholder="JohnDoe_404" required/>
         <label for="email">Password</label>
-        <input type="password" id="password" name="password" placeholder="Password" required></input>
+        <input type="password" id="password" name="password" placeholder="Password" required/>
         <small>Don't have an account? <a href='#'>Register</a></small>
         <button type="submit">Login</button>
         </form>
