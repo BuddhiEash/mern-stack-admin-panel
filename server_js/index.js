a44b50f45c2bdf96556715c6fdd7837efabe70ff
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-09-29 09:38:24 +0530

    Index updated

--- server_js/index.js
+++ server_js/index.js
@@ -1,27 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
-// import mysql from 'mysql2'
-// import bcrypt from 'bcrypt'
-import dbConnection from './components/dbconnection.js'
+import user from './components/user.js'
 
 const app = express()
 dotenv.config()
 const app_host = process.env.APP_HOST
 const app_port = process.env.APP_PORT
 
-// const connectionPool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-//   }).promise()
-
-const query = `SELECT name FROM tbl_user`
-// const userName = await connectionPool.query(query)
-const userName = await dbConnection.get().query(query)
-
 app.get('/', (req, res) => {
-  res.send(`Hello ExpressJS Server!! I'm ` + userName[0][0].name)
+  res.send(`Hello ExpressJS Server!! I'm ` + user())
 })
 
 app.listen(app_port, () => {
