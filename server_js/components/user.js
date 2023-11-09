a1916173438dd326bedd5f689e2bcd5c061616de
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-11-09 10:08:26 +0530

    User login related logic added

--- server_js/components/user.js
+++ server_js/components/user.js
@@ -1,12 +1,23 @@
 import dbConnection from './dbconnection.js'
 
 const mysqlConnection = dbConnection.getMysql()
-const query = `SELECT name FROM tbl_user`
-let userName = await mysqlConnection.query(query)
-userName = userName[0][0].name
 
-const user = () => {
-    return userName
+const user = async () => {
+    const query = `SELECT first_name FROM tbl_user LIMIT 1`
+    let firstName = await mysqlConnection.query(query)
+    firstName = firstName[0][0].first_name
+    return firstName
 }
 
-export { user }
+const login = async (username, password) => {
+    try {
+        const query = `SELECT id FROM tbl_user WHERE username = '${username}' AND password = '${password}'`
+        let id = await mysqlConnection.query(query)
+        id = id[0][0].id
+        return id ? true : false   
+    } catch (error) {
+        console.log(`Error : `, error);
+    }
+}
+
+export { user, login }
